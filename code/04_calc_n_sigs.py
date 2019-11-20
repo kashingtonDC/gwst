16c28f746af3c539a4a5ad7bed14b0017693af58
Aakash Ahamed <ahamednasa@gmail.com>
2019-11-20 09:40:35 -0800

    add better filtering

--- code/04_calc_n_sigs.py
+++ code/04_calc_n_sigs.py
@@ -2,12 +2,21 @@ import pandas as pd
 import os
 import sys
 
+# Read the file
 print('reading ' + sys.argv[1])
-
 df = pd.read_csv(sys.argv[1])
 
+# Remove space from cols
 df.columns = [x.replace(" ", "_") for x in df.columns]
 
+# Make first /lastname lowercase 
+df.First_Name = df.First_Name.str.lower()
+df.Las_Name = df.Las_Name.str.lower()
+
+# Drop duplicates
+df = df.drop_duplicates(subset=['First_Name', 'Las_Name'])
+
+# Calc statistics
 n_signatories = len(df.Las_Name.unique())
 n_countries = len(df.Country.unique())
 n_institutions = len(df.Company.unique())
